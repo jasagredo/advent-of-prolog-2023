668525ec15169784e1ee979712a4d90f03ad2701
Javier Sagredo <javier.sagredo@iohk.io>
2023-12-18 22:22:54 +0100

    risus

--- src/16.pl
+++ src/16.pl
@@ -1,13 +1,19 @@
-:- use_module(dcgs/utils).
 :- use_module(library(clpb)).
 :- use_module(library(clpz)).
 :- use_module(library(dif)).
 :- use_module(library(between)).
 :- use_module(library(debug)).
 
-matrix_nth0(X-Y, Mat, E) :-
-    nth0(Y, Mat, L),
-    nth0(X, L, E).
+matrix(_, MaxY, _, MaxY, A, A) --> [].
+matrix(MaxX, Y, MaxX, MaxY, A0, A1) -->
+    { Y1 #= Y + 1 },
+    "\n",
+    matrix(0, Y1, MaxX, MaxY, A0, A1).
+matrix(X, Y, MaxX, MaxY, A0, A1) -->
+    { X #>= 0, X #< MaxX, Y #>= 0, Y #< MaxY },
+    [Char],
+    { put_assoc(X-Y, A0, Char, A2), X1 #= X + 1 },
+    matrix(X1, Y, MaxX, MaxY, A2, A1).
 
 % reflect(IncomingDirection, Point, OutgoingDirection)
 reflect(X, '.', [X]).
@@ -31,7 +37,7 @@ next_position(left, X-Y, X1-Y) :- X1 #= X - 1.
 next_position(up, X-Y, X-Y1) :- Y1 #= Y - 1.
 next_position(down, X-Y, X-Y1) :- Y1 #= Y + 1.
 
-run(Dir-X-Y, MaxX-MaxY, Lines, MatSol, Seen, Seen1) :-
+run(Dir-X-Y, MaxX-MaxY, Lines, Seen, Seen1) :-
     next_position(Dir, X-Y, X1-Y1),
     Xz #= MaxX - 1,
     Yz #= MaxY - 1,
@@ -39,35 +45,44 @@ run(Dir-X-Y, MaxX-MaxY, Lines, MatSol, Seen, Seen1) :-
         % when inside the matrix
         X1 in 0..Xz,
         Y1 in 0..Yz,
-        % unify with 1
-        matrix_nth0(X1-Y1, MatSol, 1),
 
         % get next step
-        matrix_nth0(X1-Y1, Lines, E),
+        get_assoc(X1-Y1, Lines, E),
         reflect(Dir, E, Next),
 
         (
             % only one next direction
             Next = [Dir1],
             (
-                % if we have not seen it yet
-                maplist(dif(Dir1-X1-Y1), Seen),
-                run(Dir1-X1-Y1, MaxX-MaxY, Lines, MatSol, [Dir1-X1-Y1|Seen], Seen1)
-            ;
                 % if we have seen it, do nothing
+                get_assoc(Dir1-X1-Y1, Seen, _),
                 Seen1 = Seen
+            ;
+                % if we have not seen it yet
+                put_assoc(Dir1-X1-Y1, Seen, yes, SeenAux),
+                run(Dir1-X1-Y1, MaxX-MaxY, Lines, SeenAux, Seen1)
             )
         ;
           % two directions (a splitter)
           Next = [Dir1, Dir2],
 
-          ( maplist(dif(Dir1-X1-Y1), Seen),
-            run(Dir1-X1-Y1, MaxX-MaxY, Lines, MatSol, [Dir1-X1-Y1|Seen], Seen2)
-          ; Seen2 = Seen
+          (
+              % if we have seen it, do nothing
+              get_assoc(Dir1-X1-Y1, Seen, _),
+              Seen2 = Seen
+          ;
+              % if we have not seen it yet
+              put_assoc(Dir1-X1-Y1, Seen, yes, SeenAux1),
+              run(Dir1-X1-Y1, MaxX-MaxY, Lines, SeenAux1, Seen2)
           ),
-          ( maplist(dif(Dir2-X1-Y1), Seen),
-            run(Dir2-X1-Y1, MaxX-MaxY, Lines, MatSol, [Dir1-X1-Y1|Seen2], Seen1)
-          ; Seen1 = Seen2
+          (
+              % if we have seen it, do nothing
+              get_assoc(Dir2-X1-Y1, Seen2, _),
+              Seen1 = Seen2
+          ;
+              % if we have seen it, do nothing
+              put_assoc(Dir2-X1-Y1, Seen2, yes, SeenAux2),
+              run(Dir2-X1-Y1, MaxX-MaxY, Lines, SeenAux2, Seen1)
           )
         )
     ;
@@ -79,64 +94,41 @@ run(Dir-X-Y, MaxX-MaxY, Lines, MatSol, Seen, Seen1) :-
     ).
 
 solve(Dir-X-Y, Lines, Val) :-
-    % create matrix of unknown (yet) variables of same size
-    same_length(Lines, MatSol),
-    maplist(same_length, Lines, MatSol),
+    empty_assoc(A),
+    run(Dir-X-Y, 110-110, Lines, A, Seen),
+    assoc_to_keys(Seen, Seen2), !,
+    maplist(\ (_-B-C)^(B-C)^true, Seen2, Seen3),
+    sort(Seen3, Seen4),
+    length(Seen4, Val).
 
-    % get matrix bounds
-    length(Lines, MaxY),
-    Lines = [L|_],
-    length(L, MaxX),
-
-    % run: unify variables which the light crosses
-    run(Dir-X-Y, MaxX-MaxY, Lines, MatSol, [], _),
-
-    % any other unbound variable, set it to 0
-    maplist(maplist(\Z^(var(Z), Z = 0; true)), MatSol),
-
-    % sum the 1s
-    append(MatSol, Sols),
-    sum_list(Sols, Val).
-
-% astonishing 60 seconds to run!
 part1(File, Sol) :-
-    phrase_from_file(lines(Lines), File),
+    empty_assoc(A),
+    phrase_from_file(matrix(0, 0, 110, 110, A, Lines), File), !,
     solve(right-(-1)-0, Lines, Sol).
 
 % didn't finish it, it has to be trivially correct but wow!
 part2(File, Sol) :-
-    phrase_from_file(lines(Lines), File),
+    empty_assoc(A),
+    phrase_from_file(matrix(0, 0, 110, 110, A, Lines), File),
 
-    % get bounds
-    length(Lines, MaxY),
-    Lines = [L|_],
-    length(L, MaxX),
-
-    % enumerate indices
-    MaxX1 #= MaxX - 1,
-    numlist(0, MaxX1, Xs1),
+    numlist(0, 109, Xs1),
 
     % run the above starting on top and bottom of the matrix
-    maplist([Lines,MaxY]+\X^Val^(
-                format("X = ~d~n", [X]),
-                time(solve(down-X-(-1), Lines, Val1)),
-                time(solve(up-X-MaxY, Lines, Val2)),
-                format("~d-0: ~d~n~d-Max: ~d~n", [X, Val1, X, Val2]),
+    maplist(\X^Val^(
+                solve(down-X-(-1), Lines, Val1),
+                solve(up-X-110, Lines, Val2),
                 Val #= max(Val1, Val2)
             ),
             Xs1,
             Sols1),
 
     % enumerate indices
-    MaxY1 #= MaxY - 1,
-    numlist(0, MaxY1, Ys1),
+    numlist(0, 109, Ys1),
 
     % run the above starting on left and right of the matrix
-    maplist([Lines,MaxX]+\Y^Val^(
-                format("Y = ~d~n", [Y]),
-                time(solve(right-(-1)-Y, Lines, Val1)),
-                time(solve(left-MaxX-Y, Lines, Val2)),
-                format("0-~d: ~d~nMax-~d: ~d~n", [Y, Val1, Y, Val2]),
+    maplist(\Y^Val^(
+                solve(right-(-1)-Y, Lines, Val1),
+                solve(left-110-Y, Lines, Val2),
                 Val #= max(Val1, Val2)
             ),
             Ys1,
